011870526a29f28e30260f389b72325823e922a4
Rahultermux <rahulrajpootsaurbh@gmail.com>
2026-04-02 19:18:21 +0530

    MegaBlog project intialize

---
+++ 12MegaProject/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold text-white'>Blog App with Appwrite</h1>
+    </>
+  )
+}
+
+export default App
